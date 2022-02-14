99422e6b9fb0117865bbd8976d0142025f86004f
javrong <sunumd@yahoo.com>
2022-02-14 22:54:14 +0700

    updated commit

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
 const stock				= require('./models/stockModel.js');
+const css				= require('./public/style.css')
 
 const app = express();
 
